126c146fc22b8e6a30e87c1f56e8f704feef1bfb
eagle750 <yashwant750@gmail.com>
2018-03-24 22:14:35 +0530

    MainActivity

--- app/src/main/java/com/example/hp/kuizu/MainActivity.java
+++ app/src/main/java/com/example/hp/kuizu/MainActivity.java
@@ -2,9 +2,40 @@ package com.example.hp.kuizu;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Toolbar mToolbar;
+
+    Button startButton;
+    TextView resultTextView;
+    TextView pointsTextView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    GridLayout gridLayout;
+    int c=0;
+    int d=0,e=0,f=0;
+    int totalScore=0;
+    int ans=0;
+    String s;
+    String ch[]={"+","-","*","/"};
+    TextView sumTextView;
+    TextView timerTextView;
+    Button playAgainButton;
+    RelativeLayout gameRelativeLayout;
+    ArrayList<Integer> answers=new ArrayList<Integer>();
+    int locationOfCorrectAnswer;
+    int score=0;
+    int numberOfQuestions=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
